88ae69861faafbd686af2bc3976c9b537f05c140
nguyentien00 <149949507+nguyentien00@users.noreply.github.com>
2024-01-20 19:03:40 +0700

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,40 @@
+
+# imports necessary modules
+import pygame
+
+# ==================================================================================================================
+# CLASSES
+# ==================================================================================================================
+
+
+# class for the balls used in the game
+class ball (object):
+    def __init__(self, colour, x, y, img_name):
+        # stripes or solids
+        self.colour = colour
+        # coordinates of ball
+        self.x = x
+        self.y = y
+        # sprite of ball
+        self.sprite = pygame.image.load(img_name)
+        # direction of ball in degrees
+        self.movement_direction = 0
+        # speed of ball
+        self.speed = 0
+        # frames used in ball movement
+        self.frames = 0
+        # check for whether ball is potted
+        self.potted = False
+        self.collision_monitor = []
+        # fills the monitor list with False
+        for i in range(16):
+            self.collision_monitor.append(False)
+
+
+# class for the two player object types
+class player (object):
+    def __init__(self, number, colour):
+        self.number = number
+        self.colour = colour
+        self.only_eight_ball_left = False
+
